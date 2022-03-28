34d0a611bf5ceb2e6995d14548e4520a8914277d
EmiM <emilia.marczyk@rumblefish.pl>
2022-03-28 13:43:19 +0200

    Get proper built app name for each platform

--- .testcafe-electron-rc.js
+++ .testcafe-electron-rc.js
@@ -1,4 +1,13 @@
+const getAppName = () => {
+  const extensions = {
+    win32: 'ElectronReact Setup 4.5.0.exe',
+    linux: 'ElectronReact-4.5.0.AppImage',
+    darwin: 'ElectronReact-4.5.0.dmg'
+  }
+  return extensions[process.platform]
+}
+
 module.exports = {
   mainWindowUrl: "./release/app/dist/renderer/index.html",
-  electronPath:  "./release/build/ElectronReact-4.5.0.AppImage"
+  electronPath:  `./release/build/${getAppName()}`
 }
